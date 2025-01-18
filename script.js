0aa8929d320b0a1137f7ea3885ccc9572cf1ff6f
jeonahram88 <pouts@naver.com>
2025-01-18 17:22:33 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+﻿const searchButton = document.getElementById('searchButton');
+const searchInput = document.getElementById('searchInput');
+const results = document.getElementById('results');
+
+function showResults() {
+    if (searchInput.value.trim().toLowerCase() === '또냥이') {
+        results.style.display = 'block';
+    }
+}
+
+searchButton.addEventListener('click', showResults);
+searchInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        showResults();
+    }
+});
